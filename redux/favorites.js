07fa75a98df41cd0e187445d3fbc6a296e54f85f
Arkhypchuk Andrii <andrewarhypchuk@gmail.com>
2023-03-10 15:08:41 +0200

    redux fix

--- redux/favorites.js
+++ redux/favorites.js
@@ -3,16 +3,17 @@ import { createSlice } from "@reduxjs/toolkit";
 const FavoritesSlice =  createSlice({
     name:'favorites',
     initialState:{
-        ids:[],
-        reducers:{
-            addFavorite:(state,action)=>{
-              state.ids.push(action.payload)
-            },
-            removeFavorite:(state,action)=>{
-               state.ids.splice(state.ids.indexOf(action.payload),1)
-            }
+        ids:[],   
+    },
+    reducers:{
+        addFavorite:(state,action)=>{
+          state.ids.push(action.payload)
+        },
+        removeFavorite:(state,action)=>{
+           state.ids.splice(state.ids.indexOf(action.payload),1)
         }
     }
-})
+}
+)
 export const{addFavorite,removeFavorite} = FavoritesSlice.actions;
 export default FavoritesSlice.reducer
